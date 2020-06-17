6da323a6651fb9d2235147431c5cd3d1555e9abe
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-06-18 00:30:48 +0300

    feat; Add styling for filter

--- src/components/categoryFilter.jsx
+++ src/components/categoryFilter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './filter.scss';
 
 const categories = [
   'All',
@@ -15,9 +16,15 @@ const categories = [
 const CategoryFilter = ({ changeFilter }) => {
   const select = React.useRef(null);
   return (
-    <div>
-      <select name="category" ref={select} onChange={() => changeFilter(select.current.value)}>
-        {categories.map(category => (
+    <div className="filter">
+      <select
+        id="filter-category"
+        className="form-input"
+        name="category"
+        ref={select}
+        onChange={() => changeFilter(select.current.value)}
+      >
+        {categories.map((category) => (
           <option key={category}>{category}</option>
         ))}
       </select>
